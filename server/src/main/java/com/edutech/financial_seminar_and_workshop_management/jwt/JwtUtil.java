3bb3eae2b6a89d0c36e0304c60fe63ae0299e341
ShrutiDoshi17 <dummy@gmail.com>
2026-03-28 09:29:11 +0000

    jwtUtil done!

--- server/src/main/java/com/edutech/financial_seminar_and_workshop_management/jwt/JwtUtil.java
+++ server/src/main/java/com/edutech/financial_seminar_and_workshop_management/jwt/JwtUtil.java
@@ -1,8 +1,11 @@
 package com.edutech.financial_seminar_and_workshop_management.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 import com.edutech.financial_seminar_and_workshop_management.entity.User;
 import com.edutech.financial_seminar_and_workshop_management.repository.UserRepository;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,5 +21,50 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    private final long EXPIRATION = 1000L * 60 * 60 * 24; //1 day
+
+    public String generateToken(String username, String role){
+        return Jwts.builder()
+        .setSubject(username)
+        .claim("role", role) // Add role to token
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+        .signWith(key)
+        .compact();
+    }
+
+    public String extractAllClaims(String token){
+        return Jwts.parserBuilder()
+        .setSigningKey(key)
+        .build()
+        .parseClaimsJws(token)
+        .getBody()
+        .getSubject();
+    }
+
+    private boolean isTokenExpired(String token){
+        Date expiration = Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getExpiration();
+        return expiration.before(new Date());
+    }
+
+    public String extractUserame(String token){
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails, String username){
+       return extractUserame(token).equals(username) && !isTokenExpired(token);
+    }
+
   
-}
+} 
