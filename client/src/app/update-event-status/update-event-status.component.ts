9d51b90c0b348c8737667aecbc222f8426288128
ShrutiDoshi17 <dummy@gmail.com>
2026-03-31 05:37:42 +0000

    events

--- client/src/app/update-event-status/update-event-status.component.ts
+++ client/src/app/update-event-status/update-event-status.component.ts
@@ -11,7 +11,63 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./update-event-status.component.scss'],
   providers: [DatePipe]
 })
-export class UpdateEventStatusComponent  {
+export class UpdateEventStatusComponent implements OnInit  {
+    formModel:any={status:null};
+    showError:boolean=false;
+    errorMessage:any;
+    eventList:any=[];
+    assignModel:any={};
+    selectedEvent:any={};
+    showMessage: any;
+    reponseMessage: any;
+    updateId: any;
+    isAddRemarks:boolean=false;
+
+    ngOnInit(): void {
+      this.getEvent();
+    }
+
+    getEvent(){
+
+    }
+
+    addStatus(val:any): void{
+      this.updateId = val?.eventId;
+    }
+
+    addRemarks(val:any): void{
+      this.updateId = val?.eventId;
+      this.selectedEvent = val;
+      this.isAddRemarks = true;
+
+    }
+
+    updateStatus():void{
+      if(!this.updateId){
+        this.showError = true;
+        this.errorMessage = 'Event ID not found';
+        return;
+      }
+
+      this.assignModel.eventId = this.updateId;
+      this.assignModel.status = this.formModel.status;
+
+    }
+
+    saveFeedBack(): void{
+      if(!this.updateId){
+        this.showError = true;
+        this.errorMessage = 'Event ID not found';
+        return;
+      }
+
+      const payload={
+        eventId: this.updateId,
+        remarks: this.selectedEvent.remarks,
+        feedbackDate: new Date()
+      };
+
+    }
  
   
   
--- client/src/app/view-events/view-events.component.ts
+++ client/src/app/view-events/view-events.component.ts
@@ -12,7 +12,55 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./view-events.component.scss'],
   providers: [DatePipe]
 })
-export class ViewEventsComponent  {
+export class ViewEventsComponent implements OnInit  {
+  formModel:any={status:null};
+  showError:boolean=false;
+  errorMessage:any;
+  eventObj:any=[];
+  assignModel:any={};
+  showMessage:any;
+  responseMessage:any;
+  isUpdate:any=false;
+  eventList:any=[];
+  workShopList:any=[];
+  userId:any;
+  selectedEvent:any={};
+  status:any;
+
+  ngOnInit(): void {
+    this.userId = localStorage.getItem('userId');
+    this.getEvent();
+    
+  }
+
+  getEvent(){
+
+  }
+
+  enroll(){
+
+  }
+
+  viewDetails(val:any){
+    this.selectedEvent = val;
+
+  }
+
+  saveFeedBack(){
+    if(!this.selectedEvent || this.formModel.feedback){
+      this.showError = true;
+      this.errorMessage = 'Feedback cannot be empty';
+      return;
+    }
+
+    this.formModel.timestamp = new Date();
+
+  }
+
+  checkStatus(){
+
+  }
+
 
    
   
